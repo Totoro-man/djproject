07f631bcc91c17905f73023b40ae76cf0b031e90
Тоторо <roman.aka.tod@gmail.com>
2024-05-12 22:34:32 +0300

    20.1 Ex 4 Start Add 'manufactured_at' field

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    caption = models.CharField(max_length=30, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.caption}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('caption',)
+
+
+class Product(models.Model):
+    caption = models.CharField(max_length=30, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    view = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    manufactured_at = models.DateTimeField(**NULLABLE)
+
+    def __str__(self):
+        return f'{self.caption} ({self.category}) : {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('caption',)
+
